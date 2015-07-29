2f1942e0741118172eb76ba3894069f8e464585e
Johnathan McCabe <johnathan.mccabe@gmail.com>
2015-07-29 13:40:22 +0100

    Added API functions

--- R/data_info_team.R
+++ R/data_info_team.R
@@ -15,7 +15,7 @@
 #' - update_date
 #' @examples
 #' get_info_team_query_from_meta(query_id = "RenewalStatus")
-get_info_team_query_from_meta <- function(server = "GBLONTPD57", 
+get_info_team_query_from_meta <- function(server = "FARADAYWAREHOUSE", 
                                           database = "FaradayInfoTeam", 
                                           user ="", 
                                           password = "",
@@ -67,16 +67,16 @@ get_info_team_query_from_meta <- function(server = "GBLONTPD57",
 #'    inception_to = "2015-08-01", 
 #'    reporting_level = "team",
 #'    reporting_values = "ca")
-get_info_team_renewal_status <- function(server = "GBLONTPD06",
-                                         database = "WWD",
-                                         user = "",
-                                         password = "",
-                                         syndicate = c(1192,435),
-                                         inception_from,
+get_info_team_renewal_status <- function(inception_from,
                                          inception_to,
                                          reporting_level = "team",
-                                         reporting_values){
+                                         reporting_values,
+                                         syndicate = c(1192,435)){
   
+  server = "GBLONTPD06"
+  database = "WWD"
+  user = ""
+  password = ""
   
   ## exchange rates
   
@@ -114,6 +114,116 @@ get_info_team_renewal_status <- function(server = "GBLONTPD06",
     paste0('driver={SQL Server};server=',server,';database=',database,';Trusted_Connection = Yes')
   }
 
+  con <- odbcDriverConnect(conn_string)
+  
+  ## execute query
+  data <- sqlExecute(con, query = query, fetch = T, stringsAsFactors = F)
+  
+  ## close connection
+  odbcClose(con)
+  
+  return(data)
+}
+
+
+#' Returns the renewal information for the selected inception and expiry date ranges
+#' 
+#'
+#' @param syndicate Vector containing syndicate IDs
+#' @param inception_from First inception date
+#' @param inception_to Latest inception date
+#' @param reporting_level The reporting level to query at - options are 'class', 'minorclass' or 'aatg'
+#' @param reporting_values Vector of reporting level values
+#' @return A dataset with risk information
+#' @examples
+#' get_info_team_policy_info(2013,2015,"class", "ca")
+get_info_team_policy_info<- function(yoa_from,
+                                       yoa_to,
+                                       reporting_level = "class",
+                                       reporting_values,
+                                       syndicate = c(1192,435)){
+  
+  server = "FARADAYWAREHOUSE"
+  database = "FaradayInfoTeam"
+  user = ""
+  password = ""
+  
+  ## set up query with parameters
+  r_level <- tolower(reporting_level)
+  level <- ifelse(r_level == "class", "fsg.classtype", 
+                  ifelse(r_level == "minorclass",
+                         "fsg.minorclass",
+                         "fsr.actuarialatg"))
+  
+  values <- paste0("'",reporting_values,"'", collapse=",")
+  synd <- paste(as.character(syndicate), collapse=",")
+
+  query <- "SELECT
+
+  fsg.Classtype 
+  , fsg.majorclassDesc
+  , fsg.minorclass
+  , fsr.actuarialATG as AATG
+  , FSR.SyndRef
+  , FSR.RiskRef
+  , fsr.PrevRiskRef
+  , fsr.PrevYoA
+  , fsr.leadind
+  , fsr.LeadUW
+  , fsr.LeadLine
+  , FSR.UWDetailsComplete
+  , FSR.CurrUw
+  , FSR.YoA
+  , FSr.Block
+  , FSr.Stat
+  , FSR.InsType
+  , fsr.InsTypeDesc
+  , FSR.PeriodType
+  , FSR.Assured
+  , FSR.Programref
+  , FSR.InceptionDate
+  , FSR.ExpiryDate
+  , FSR.LineStatus
+  , FSR.LimitCCYiso
+  , FSR.Limit
+  , FSR.Deductible
+  , FSR.SignedLine
+  , FSR.SignedOrder
+  , FSR.WrittenLine
+  , FSR.EstSignedLine
+  
+  
+  
+  FROM  
+  
+  FaradayWarehouse.dbo.FW_D_Risk as FSR
+  
+  LEFT OUTER JOIN
+  FaradayReporting.dbo.reporting as FSG
+  ON FSR.BlockStat = FSG.Class AND FSR.SyndRef = FSG.SyndRef
+  
+  
+  WHERE 
+  
+  FSR.Syndref in (&Synd&)
+  and FSR.YoA between  &YearFrom& and &YearTo&
+  AND  &Where& in (&WhereValue&) 
+  and fsr.actuarialATG not like '9%'"
+  
+  query <- str_replace_all(query, "&AsAt&", as_at)
+  query <- str_replace_all(query, "&YearFrom&", yoa_from)
+  query <- str_replace_all(query, "&YearTo&", yoa_to)
+  query <- str_replace_all(query, "&Synd&", synd)
+  query <- str_replace_all(query, "&Where&", level)
+  query <- str_replace_all(query, "&WhereValue&", values)
+  
+  ## set up connection
+  conn_string <- if(user != ""){
+    paste0('driver={SQL Server};server=',server,';database=',database,';uid=', user,';pwd=',password)
+  }else{
+    paste0('driver={SQL Server};server=',server,';database=',database,';Trusted_Connection = Yes')
+  }
+  
   con <- odbcDriverConnect(conn_string)
   
   ## execute query
--- R/data_spec_lines_model.R
+++ R/data_spec_lines_model.R
@@ -2,7 +2,14 @@
 
 
 
-
+#' Returns the priced layers
+#' 
+#'
+#' @param quote_id The quote ID
+#' @return A string containing the url to the model
+#' @details The function is vectorised over quote_id
+#' @examples
+#' get_model_url(c(345,762))
 get_priced_layers <- function(inception_from, inception_to){
   
   conn_string <- 'driver={SQL Server};server=gblontpd54;database=pricing_prod;uid=pricingteam;pwd=F4raday00'
@@ -54,4 +61,149 @@ get_priced_layers <- function(inception_from, inception_to){
   
   return(report.data)
 }
+
+#' Returns the url for the quote id from the model
+#' 
+#'
+#' @param quote_id The quote ID
+#' @return A string containing the url to the model
+#' @details The function is vectorised over quote_id
+#' @examples
+#' get_model_url(c(345,762))
+get_model_url <- function(quote_id){
+  
+  base_url <- "http://uwportal/Pricing.UI.Model.Splines/Quotes/Versions?quoteId=%quote_id%&modelCode=SPL"
+  
+  retVal <- sapply(quote_id, function(q){
+    str_replace(base_url, "%quote_id%", as.character(q))
+  })
+  
+  return(retVal)
+}
+
+#' Returns a list of layers from accounts where the Sequel ID doesn't appear to be correct
+#' 
+#'
+#' @param quote_id The quote ID
+#' @return A dataframe with details of layers with strange Sequel IDs
+#' @details The following methods are used to check Sequel ID integrity:
+#' - Sequel ID pattern matching
+#' - Ensure Sequel ID year = Inception year
+#' @examples
+#' get_funky_sequel_matches()
+get_funky_sequel_matches <- function(){
+  conn_string <- 'driver={SQL Server};server=gblontpd54;database=pricing_prod;uid=pricingteam;pwd=F4raday00'
+  
+  ## set up connection
+  con <- odbcDriverConnect(conn_string)
+  
+  query <- "select
+  
+  l.sequel_id,
+  l.layer_no,
+  s.limit,
+  s.deductible,
+  a.assured_name,
+  qv.quote_id,
+  qv.quote_version_no,
+  qv.inception_date
+  
+  from dbo.quote_version qv
+  inner join dbo.layer l on l.quote_version_id = qv.quote_version_id
+  inner join dbo.section s on s.layer_id = l.layer_id
+  inner join dbo.quote_master qm on qv.quote_id = qm.quote_id
+  inner join ref.assured a on a.assured_id = qm.assured_id
+  
+  where
+  qv.is_deleted = 'N'
+  and l.is_deleted = 'N'
+  and l.sequel_id is not null
+  and s.section_letter = 'A'
+  and s.is_deleted = 'N'
+  and qv.is_selected_final = 'Y'"
+  
+  ## execute query
+  model.data <- sqlExecute(con, query = query, fetch = T, stringsAsFactors = F)
+  
+  ## close connection
+  odbcClose(con)
+  
+  
+  ## Various checks on Sequel IDs
+  sequel_regex <- "([A-Za-z]{2})([0-9]{3})([A-Za-z])([0-9]{2})([A-Za-z][0-9]{3})"
+  
+  ## suggestion Sequel ID
+  model.data$suggestion <- NA
+  
+  ## Check inception year against year from Sequel ID
+  year <- as.character(as.POSIXlt(model.data$inception_date)$year - 100)
+  sequel_years <- str_match(model.data$sequel_id,sequel_regex)
+  
+  year_match <- (year != sequel_years[,5]) & (!is.na(sequel_years[,5]))
+  
+  model.data$suggestion[year_match] <- paste0(sequel_years[,2],
+                                              sequel_years[,3],
+                                              sequel_years[,4],
+                                              year,
+                                              sequel_years[,6])[year_match]
+  
+  output <- model.data[year_match,] %>%
+    mutate(method = "Inconsistent Year",
+           url = get_model_url(quote_id))
+  
+  ## Check doesn't match pattern
+  pat_match <- is.na(sequel_years[,1])
+  
+
+  ## suggestions from info team - match inception, assured and limit/deductible
+  yoa_from <- min(as.POSIXlt(model.data$inception_date)$year + 1900)
+  yoa_to <- max(as.POSIXlt(model.data$inception_date)$year + 1900)
+  
+  info.data <- get_info_team_policy_info(yoa_from = yoa_from,
+                                         yoa_to,
+                                         reporting_values = "ca") %>%
+    filter(Assured != "") %>%
+    ## tz ="" so as not to mess up dates!!!
+    mutate(incept_date = as.Date(InceptionDate, tz="")) %>%
+    select(Assured,incept_date,Limit,Deductible, RiskRef) %>%
+    group_by(Assured,incept_date,Limit,Deductible) %>%
+    ## can be a sequel_id for each blockstat - just take the first as this is only to offer
+    ## a suggestion
+    distinct()
+    
+
+  match.data <- model.data %>%
+    ## tz ="" so as not to mess up dates!!!
+    mutate(incept_date = as.Date(inception_date, tz="")) %>%
+    left_join(info.data,
+              by = c("assured_name" = "Assured", 
+                                 "incept_date",
+                                 "limit" = "Limit",
+                                 "deductible" = "Deductible"))
+# 
+#   
+#   View(x<-match.data %>%
+#          group_by(id) %>%
+#          summarise(count=n()) %>%
+#          arrange(desc(count)))
+#     
+#   match.data %>% filter(id == 418)
+#   
+#   model.data %>% filter(id == 236)
+#   
+  info.data %>% filter(Assured == "HIGHMARK CASUALTY INS CO")
+  
+  model.data$suggestion[pat_match] <- match.data$RiskRef[pat_match]   
+  
+  ## add to output
+  output <- output %>%
+    rbind(model.data[pat_match,] %>%
+            mutate(method = "Invalid Sequel ID",
+                   url = get_model_url(quote_id)))
+  
+  
+  return(output)
+}
+
+
 
